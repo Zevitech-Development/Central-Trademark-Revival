dd917b433244f8c60d537e40f5dba43a98483100
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-05-08 22:48:29 +0500

    fix build error 02

--- src/app/(business)/checkout/[formId]/page.tsx
+++ src/app/(business)/checkout/[formId]/page.tsx
@@ -5,15 +5,16 @@ import CheckoutPage from "@/customs/checkout-page";
 
 import { GetPageMetadata } from "@/utils/meta-data";
 
-type CheckoutParams = {
-  formId: string;
-};
+interface PageProps {
+  params: {
+    formId: string;
+  };
+  searchParams?: Record<string, string | string[] | undefined>;
+}
 
 export const generateMetadata = async ({
   params,
-}: {
-  params: CheckoutParams;
-}): Promise<Metadata> => {
+}: PageProps): Promise<Metadata> => {
   return GetPageMetadata({
     title: `Checkout #${params.formId} | Finalize Your Trademark Revival - Central Trademark Revival®`,
     description:
@@ -21,11 +22,7 @@ export const generateMetadata = async ({
   });
 };
 
-export default function Checkout({
-  params,
-}: {
-  params: CheckoutParams;
-}): React.JSX.Element {
+export default function Checkout({ params }: PageProps): React.JSX.Element {
   const { formId } = params;
 
   return <CheckoutPage formId={formId} />;
