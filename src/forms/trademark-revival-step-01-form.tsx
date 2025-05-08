df5f5b059860d7b1ef15db64227a9ee2e533fff3
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-05-09 01:04:26 +0500

    fix build error 04

--- src/forms/trademark-revival-step-01-form.tsx
+++ src/forms/trademark-revival-step-01-form.tsx
@@ -30,6 +30,7 @@ import { FaCircleCheck, FaCircleXmark } from "react-icons/fa6";
 
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     grecaptcha: any;
   }
 }
